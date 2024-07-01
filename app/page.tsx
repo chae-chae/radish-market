8a1f911c7645e1ef7b7bd2d1c0ebc0014318df7f
Jeong Chae Kyeong <chaechae.d3v@gmail.com>
2024-07-01 16:30:04 +0900

    test tailwindCSS variables

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,11 @@ export default function Home() {
     <main className="bg-gray-100 h-screen flex items-center justify-center p-5 ">
       <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col gap-2">
         <input
-          className="w-full rounded-full h-10 bg-gray-200 pl-5 outline-none ring ring-orange-500 ring-offset-2 ring-offset-blue-600"
+          className="w-full rounded-full h-10 bg-gray-200 pl-5 outline-none ring ring-transparent focus:ring-orange-500 focus:ring-offset-2 transition-shadow placeholder:drop-shadow"
           type="text"
           placeholder="Search here..."
         />
-        <button className="bg-black text-white py-2 rounded-full active:scale-90 focus:scale-90 transition-transform font-medium outline-none">
+        <button className="bg-black text-white py-2 rounded-full active:scale-90 transition-transform font-medium outline-none">
           Search
         </button>
       </div>
