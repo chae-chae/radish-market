abf877a36ba404d29e6f27148e5d00dac38df1d6
Jeong Chae Kyeong <chaechae.d3v@gmail.com>
2024-07-02 14:46:39 +0900

    tailwindCSS animate - spin

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,13 @@
 export default function Home() {
   return (
     <main className="bg-gray-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 h-screen flex items-center justify-center p-5 ">
-      <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col gap-4">
+      <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col gap-3">
         {["Jeong", "Chae", "Kyeong", "You"].map((person, index) => (
-          <div
-            key={index}
-            className="flex items-center gap-5 border-b-2 pb-5 last:pb-0 last:border-0"
-          >
+          <div key={index} className="flex items-center gap-5 ">
             <div className="size-10 bg-blue-400 rounded-full" />
             <span className="text-lg font-medium">{person}</span>
-            <div className="size-6 bg-red-500 text-white flex items-center justify-center rounded-full">
-              <span>{index}</span>
+            <div className="size-6 animate-spin flex items-center justify-center rounded-full">
+              <span>⏳</span>
             </div>
           </div>
         ))}
