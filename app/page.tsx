0618ccda29663d2b19a09ad0a9bf1712a9cbf32c
Jeong Chae Kyeong <chaechae.d3v@gmail.com>
2024-07-02 14:58:15 +0900

    tailwindCSS animate-ping

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,13 @@ export default function Home() {
     <main className="bg-gray-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 h-screen flex items-center justify-center p-5 ">
       <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col gap-3">
         {["Jeong", "Chae", "Kyeong", "You"].map((person, index) => (
-          <div key={index} className="flex items-center gap-5 *:animate-pulse">
+          <div key={index} className="flex items-center gap-5 ">
             <div className="size-10 bg-blue-400 rounded-full" />
-            <div className="w-40 h-3 rounded-full bg-gray-400" />
-            <div className="w-20 h-3 rounded-full bg-gray-400" />
+            <span className="text-lg font-medium">{person}</span>
+            <div className="size-6 bg-red-500 text-white flex items-center justify-center rounded-full relative">
+              <span className="z-10">{index}</span>
+              <div className="size-6 bg-red-500 rounded-full absolute animate-ping" />
+            </div>
           </div>
         ))}
       </div>
