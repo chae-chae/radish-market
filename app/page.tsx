074f2ea54f96f40a59463a023af8cdc69a67ddde
Jeong Chae Kyeong <chaechae.d3v@gmail.com>
2024-07-02 14:55:01 +0900

    add skeleton using tailwindCSS animate-pulse

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,10 @@ export default function Home() {
     <main className="bg-gray-100 sm:bg-red-100 md:bg-green-100 lg:bg-cyan-100 h-screen flex items-center justify-center p-5 ">
       <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col gap-3">
         {["Jeong", "Chae", "Kyeong", "You"].map((person, index) => (
-          <div key={index} className="flex items-center gap-5 ">
+          <div key={index} className="flex items-center gap-5 *:animate-pulse">
             <div className="size-10 bg-blue-400 rounded-full" />
-            <span className="text-lg font-medium">{person}</span>
-            <div className="size-6 animate-spin flex items-center justify-center rounded-full">
-              <span>⏳</span>
-            </div>
+            <div className="w-40 h-3 rounded-full bg-gray-400" />
+            <div className="w-20 h-3 rounded-full bg-gray-400" />
           </div>
         ))}
       </div>
