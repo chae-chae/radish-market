cb27c707ed72064148c235e1726bf6726ba5a871
Jeong Chae Kyeong <chaechae.d3v@gmail.com>
2024-07-02 14:42:04 +0900

    first & last modifiers

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
         {["Jeong", "Chae", "Kyeong", "You"].map((person, index) => (
           <div
             key={index}
-            className="flex items-center gap-5 odd:bg-gray-100 even:bg-cyan-100 p-2.5 rounded-xl"
+            className="flex items-center gap-5 border-b-2 pb-5 last:pb-0 last:border-0"
           >
             <div className="size-10 bg-blue-400 rounded-full" />
             <span className="text-lg font-medium">{person}</span>
