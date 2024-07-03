b2a38166af43d0305cd293d83cd508597662bcdc
Jeong Chae Kyeong <chaechae.d3v@gmail.com>
2024-07-03 22:26:21 +0900

    route handlers

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,20 @@
+"use client";
+
 import FormButton from "@/components/form-btn";
 import FormInput from "@/components/form-input";
 import SocialLogin from "@/components/social-login";
 
 export default function LogIn() {
+  const onClick = async () => {
+    const response = await fetch("/www/users", {
+      method: "POST",
+      body: JSON.stringify({
+        username: "chaechae",
+        password: "123",
+      }),
+    });
+    console.log(await response.json());
+  };
   return (
     <div className="flex flex-col gap-10 px-6 py-8">
       <div className="flex flex-col gap-2 *:font-medium">
@@ -17,8 +29,10 @@ export default function LogIn() {
           placeholder="password"
           errors={[]}
         />
-        <FormButton loading={false} text="계정 생성" />
       </form>
+      <span onClick={onClick}>
+        <FormButton loading={false} text="로그인" />
+      </span>
       <SocialLogin />
     </div>
   );
