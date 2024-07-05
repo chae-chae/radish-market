4049a2ce64b52f0c37ef984e644cac5a972ec5fc
Jeong Chae Kyeong <chaechae.d3v@gmail.com>
2024-07-05 20:40:08 +0900

    add sms/actions.ts

--- app/login/actions.ts
+++ app/login/actions.ts
@@ -1,17 +1,29 @@
 "use server";
 
-export async function handleForm(prevState: any, formData: FormData) {
-  await new Promise((resolve) => setTimeout(resolve, 2000));
-  console.log(prevState);
-  console.log("logged in");
-  if (formData.get("password") === "12345") {
-    return {
-      errors: [],
-      success: true,
-    };
+import {
+  PASSWORD_MIN_LENGTH,
+  PASSWORD_REGEX,
+  PASSWORD_REGEX_ERROR,
+} from "@/lib/constants";
+import { z } from "zod";
+
+const formSchema = z.object({
+  email: z.string().email().toLowerCase(),
+  password: z
+    .string({ required_error: "Password is a required" })
+    .min(PASSWORD_MIN_LENGTH)
+    .regex(PASSWORD_REGEX, PASSWORD_REGEX_ERROR),
+});
+
+export async function login(prevState: any, formData: FormData) {
+  const data = {
+    email: formData.get("email"),
+    password: formData.get("password"),
+  };
+  const result = formSchema.safeParse(data);
+  if (!result.success) {
+    return result.error.flatten();
   } else {
-    return {
-      errors: ["wrong password"],
-    };
+    console.log(result.data);
   }
 }
