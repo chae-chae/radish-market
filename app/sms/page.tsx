0dd4c9a6a061c4b3e3c5f62c67bd53815caacbc5
Jeong Chae Kyeong <chaechae.d3v@gmail.com>
2024-07-05 20:44:09 +0900

    edit smslogin

--- app/sms/page.tsx
+++ app/sms/page.tsx
@@ -2,10 +2,10 @@
 import Button from "@/components/button";
 import Input from "@/components/input";
 import { useFormState } from "react-dom";
-import { smsVerification } from "./actions";
+import { smsLogin } from "./actions";
 
 export default function SMSLogin() {
-  const [state, dispatch] = useFormState(smsVerification, null);
+  const [state, dispatch] = useFormState(smsLogin, null);
   return (
     <div className="flex flex-col gap-10 py-8 px-6">
       <div className="flex flex-col gap-2 *:font-medium">
