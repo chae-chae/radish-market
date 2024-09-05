f4a8d77bdbd9966e736784cc1734352df04ec9e8
Jeong Chae Kyeong <chaechae.d3v@gmail.com>
2024-09-05 11:53:29 +0900

    add logging

--- app/extras/page.tsx
+++ app/extras/page.tsx
@@ -1,8 +1,24 @@
-export default function Extras() {
+import { revalidatePath } from "next/cache";
+
+async function getData() {
+  const data = await fetch(
+    "https://nomad-movies.nomadcoders.workers.dev/movies"
+  );
+}
+
+export default async function Extras() {
+  await getData();
+  const action = async () => {
+    "use server";
+    revalidatePath("/extras");
+  };
   return (
     <div className="flex flex-col gap-3 py-10">
       <h1 className="text-6xl font-metallica">Extras!</h1>
       <h2 className="font-roboto">So much more to learn!</h2>
+      <form action={action}>
+        <button>revalidate</button>
+      </form>
     </div>
   );
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
   images: {
     remotePatterns: [
       {
         hostname: "avatars.githubusercontent.com",
+      },
+      {
+        hostname: "imagedelivery.net",
       },
     ],
   },
