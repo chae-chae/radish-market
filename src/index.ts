35eb2c635aae6124dae74facdeb00708260bba4e
Jeong Chae Kyeong <chaechae.d3v@gmail.com>
2024-09-09 10:14:03 +0900

    add index.ts

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+export interface Env { // Example binding to KV. Learn more at
+https://developers.cloudflare.com/workers/runtime-apis/kv/ DB: KVNamespace; //
+// Example binding to Durable Object. Learn more at
+https://developers.cloudflare.com/workers/runtime-apis/durable-objects/ //
+MY_DURABLE_OBJECT: DurableObjectNamespace; // // Example binding to R2. Learn
+more at https://developers.cloudflare.com/workers/runtime-apis/r2/ // MY_BUCKET:
+R2Bucket; } // @ts-ignore import home from "./home.html"; export default { async
+fetch( request: Request, env: Env, ctx: ExecutionContext ): Promise<Response>
+  { const url = new URL(request.url); if (url.pathname === "/") { await
+  env.DB.put("hello", "how are you?"); return new Response(home, { headers: {
+  "Content-Type": "text/html;chartset=utf-8", }, }); } return new Response(null,
+  { status: 404, }); }, };</Response
+>
