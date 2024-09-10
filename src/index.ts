640b7c16210e1cf387fb969ed766822d0dee8a83
Jeong Chae Kyeong <chaechae.d3v@gmail.com>
2024-09-10 10:10:11 +0900

    add visit counter

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,71 @@
-export interface Env { // Example binding to KV. Learn more at
-https://developers.cloudflare.com/workers/runtime-apis/kv/ DB: KVNamespace; //
-// Example binding to Durable Object. Learn more at
-https://developers.cloudflare.com/workers/runtime-apis/durable-objects/ //
-MY_DURABLE_OBJECT: DurableObjectNamespace; // // Example binding to R2. Learn
-more at https://developers.cloudflare.com/workers/runtime-apis/r2/ // MY_BUCKET:
-R2Bucket; } // @ts-ignore import home from "./home.html"; export default { async
-fetch( request: Request, env: Env, ctx: ExecutionContext ): Promise<Response>
-  { const url = new URL(request.url); if (url.pathname === "/") { await
-  env.DB.put("hello", "how are you?"); return new Response(home, { headers: {
-  "Content-Type": "text/html;chartset=utf-8", }, }); } return new Response(null,
-  { status: 404, }); }, };</Response
->
+export interface Env {
+  // Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
+  DB: KVNamespace;
+  //
+  // Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
+  // MY_DURABLE_OBJECT: DurableObjectNamespace;
+  //
+  // Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
+  // MY_BUCKET: R2Bucket;
+}
+
+// @ts-ignore
+import home from "./home.html";
+
+function handleHome() {
+  return new Response(home, {
+    headers: {
+      "Content-Type": "text/html;chartset=utf-8",
+    },
+  });
+}
+
+function handleNotFound() {
+  return new Response(null, {
+    status: 404,
+  });
+}
+
+function handleBadRequest() {
+  return new Response(null, {
+    status: 400,
+  });
+}
+
+async function handleVisit(searchParams: URLSearchParams, env: Env) {
+  const page = searchParams.get("page");
+  if (!page) {
+    return handleBadRequest();
+  }
+  const kvPage = await env.DB.get(page);
+  let value = 1;
+  if (!kvPage) {
+    await env.DB.put(page, value + "");
+  } else {
+    value = parseInt(kvPage) + 1;
+    await env.DB.put(page, value + "");
+  }
+  return new Response(JSON.stringify({ visits: value }), {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+}
+
+export default {
+  async fetch(
+    request: Request,
+    env: Env,
+    ctx: ExecutionContext
+  ): Promise<Response> {
+    const { pathname, searchParams } = new URL(request.url);
+    switch (pathname) {
+      case "/":
+        return handleHome();
+      case "/visit":
+        return handleVisit(searchParams, env);
+      default:
+        return handleNotFound();
+    }
+  },
+};
