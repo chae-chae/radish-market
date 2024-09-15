5b4bb54822eea22a0058fca15e5d38939774bb98
Jeong Chae Kyeong <chaechae.d3v@gmail.com>
2024-09-15 20:41:10 +0900

    fix actions.ts

--- app/products/add/actions.ts
+++ app/products/add/actions.ts
@@ -1,46 +1,70 @@
 "use server";
 
+import { z } from "zod";
 import db from "@/lib/db";
 import getSession from "@/lib/session";
 import { redirect } from "next/navigation";
-import { z } from "zod";
 
-const title = z.string();
+const productSchema = z.object({
+  photo: z.string({
+    required_error: "Photo is required",
+  }),
+  title: z.string({
+    required_error: "Title is required",
+  }),
+  description: z.string({
+    required_error: "Description is required",
+  }),
+  price: z.coerce.number({
+    required_error: "Price is required",
+  }),
+});
 
-export async function startStream(_: any, formData: FormData) {
-  const results = title.safeParse(formData.get("title"));
-  if (!results.success) {
-    return results.error.flatten();
+export async function uploadProduct(_: any, formData: FormData) {
+  const data = {
+    photo: formData.get("photo"),
+    title: formData.get("title"),
+    price: formData.get("price"),
+    description: formData.get("description"),
+  };
+  const result = productSchema.safeParse(data);
+  if (!result.success) {
+    return result.error.flatten();
+  } else {
+    const session = await getSession();
+    if (session.id) {
+      const product = await db.product.create({
+        data: {
+          title: result.data.title,
+          description: result.data.description,
+          price: result.data.price,
+          photo: result.data.photo,
+          user: {
+            connect: {
+              id: session.id,
+            },
+          },
+        },
+        select: {
+          id: true,
+        },
+      });
+      redirect(`/products/${product.id}`);
+      //redirect("/products")
+    }
   }
+}
+
+export async function getUploadUrl() {
   const response = await fetch(
-    `https://api.cloudflare.com/client/v4/accounts/${process.env.CLOUDFLARE_ACCOUNT_ID}/stream/live_inputs`,
+    `https://api.cloudflare.com/client/v4/accounts/${process.env.CLOUDFLARE_ACCOUNT_ID}/images/v2/direct_upload`,
     {
       method: "POST",
       headers: {
         Authorization: `Bearer ${process.env.CLOUDFLARE_API_KEY}`,
       },
-      body: JSON.stringify({
-        meta: {
-          name: results.data,
-        },
-        recording: {
-          mode: "automatic",
-        },
-      }),
     }
   );
   const data = await response.json();
-  const session = await getSession();
-  const stream = await db.liveStream.create({
-    data: {
-      title: results.data,
-      stream_id: data.result.uid,
-      stream_key: data.result.rtmps.streamKey,
-      userId: session.id!,
-    },
-    select: {
-      id: true,
-    },
-  });
-  redirect(`/streams/${stream.id}`);
+  return data;
 }
