42edd6ee2cd11150811a279ab6d8e9a7a61d4491
Jeong Chae Kyeong <chaechae.d3v@gmail.com>
2024-10-05 09:46:58 +0900

    fix routes under (auth) directory

--- app/(auth)/page.tsx
+++ app/(auth)/page.tsx
@@ -1,15 +1,14 @@
 import Link from "next/link";
-import "@/lib/db";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-between min-h-screen p-5">
+    <div className="flex flex-col items-center justify-between min-h-screen p-6">
       <div className="my-auto flex flex-col items-center gap-2 *:font-medium">
         <span className="text-9xl">🥕</span>
-        <h1 className="text-4xl">당근</h1>
-        <h2 className="text-2xl">당근 마켓에 어서 오세요!</h2>
+        <h1 className="text-4xl ">당근</h1>
+        <h2 className="text-2xl">당근 마겟에 어서오세요!</h2>
       </div>
-      <div className="w-full flex flex-col items-center gap-3">
+      <div className="flex flex-col items-center gap-3 w-full">
         <Link href="/create-account" className="primary-btn text-lg py-2.5">
           시작하기
         </Link>
